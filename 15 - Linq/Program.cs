eafc664d437d581de4f6d98bbe5e1c0caa533dc3
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-14 08:25:07 +0100

    Add project files.

---
+++ 15 - Linq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15___Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
